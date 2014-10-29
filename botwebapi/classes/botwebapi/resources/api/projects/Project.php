9b19fd4739d4eeff4a0079de9602d3795027f914
Stefan Zeltner <zeltner@usc.edu>
2014-10-29 14:40:25 +0000

    implement GET /api/projects/<project>

--- botwebapi/classes/botwebapi/resources/api/projects/Project.php
+++ botwebapi/classes/botwebapi/resources/api/projects/Project.php
@@ -26,7 +26,21 @@ class Project extends resources\BotWebApiResource
     
     protected function handleGetRequest()
     {
-        return new botwebapi\JsonHttpResponse(405, $_SERVER['REQUEST_METHOD'].' is not supported');
+        $links = new botwebapi\LinksObject();
+        $links->addLink($this->getUri(), 'self');
+        
+        foreach (glob(__DIR__.'/*') as $file)
+        {
+            // the child-resources of <project> are always located in a directory
+            if(is_dir($file) && !is_link($file))
+            {
+                $child_resource_name = basename($file);
+                $links->addLink($this->getUri().'/'.$child_resource_name, 'child-resources', false);
+            }
+        }
+        
+        return new botwebapi\JsonHttpResponse(200, array('about' => new botwebapi\AboutObject($this),
+                                                         'links' => $links));
     }
     
     protected function handlePostRequest()
@@ -50,9 +64,19 @@ class Project extends resources\BotWebApiResource
             return new botwebapi\JsonHttpResponse(404, 'Could not delete '.$this->location);
         }
     }
+    
     protected function getChild($name)
     {
-        return NULL;
+        try
+        {
+            // Load the resource. The class name is <this namespace>\<name>\<Name>
+            $resource_class_name = __NAMESPACE__.'\\'.$name.'\\'.ucfirst($name);
+            return new $resource_class_name($name, $this->getUri().'/'.$name);
+        }
+        catch(\Exception $e)
+        {
+            return NULL;
+        }
     }
 }
 
---
+++ botwebapi/classes/botwebapi/resources/api/projects/binaries/Binaries.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace botwebapi\resources\api\projects\binaries;
+use botwebapi\resources as resources;
+use botwebapi as botwebapi;
+
+class Binaries extends resources\BotWebApiResource
+{
+    public function __construct($name, $uri)
+    {
+        parent::__construct($name, $uri, '1.0', 'https://github.com/kipr/botwebapi');
+        $this->location = $location;
+    }
+    
+    protected function handleGetRequest()
+    {
+        return new botwebapi\JsonHttpResponse(501, 'Not implemented yet');
+    }
+    
+    protected function handlePostRequest()
+    {
+        return new botwebapi\JsonHttpResponse(501, 'Not implemented yet');
+    }
+    
+    protected function handlePutRequest()
+    {
+        return new botwebapi\JsonHttpResponse(405, $_SERVER['REQUEST_METHOD'].' is not supported');
+    }
+    
+    protected function handleDeleteRequest()
+    {
+        return new botwebapi\JsonHttpResponse(501, 'Not implemented yet');
+    }
+    
+    protected function getChild($name)
+    {
+        return NULL;
+    }
+}
+
+?>
