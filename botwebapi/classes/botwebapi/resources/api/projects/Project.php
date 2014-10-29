c319510e3e73e90cd474b3baeab1e676d8777d11
Stefan Zeltner <zeltner@usc.edu>
2014-10-29 00:19:56 +0000

    implement 'list projects'

---
+++ botwebapi/classes/botwebapi/resources/api/projects/Project.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace botwebapi\resources\api\projects;
+use botwebapi\resources as resources;
+use botwebapi as botwebapi;
+
+class Project extends resources\BotWebApiResource
+{
+    private $location = '';
+    
+    public function __construct($name, $uri, $location)
+    {
+        if(!is_readable($location))
+        {
+            throw new \Exception('Invalid argument: $location');
+        }
+        
+        parent::__construct($name, $uri, '1.0', 'https://github.com/kipr/botwebapi');
+        $this->location = $location;
+    }
+    
+    public function getLocation()
+    {
+        return $this->location;
+    }
+    
+    protected function handleGetRequest()
+    {
+        return new botwebapi\JsonHttpResponse(405, $_SERVER['REQUEST_METHOD'].' is not supported');
+    }
+    
+    protected function handlePostRequest()
+    {
+        return new botwebapi\JsonHttpResponse(405, $_SERVER['REQUEST_METHOD'].' is not supported');
+    }
+    
+    protected function handlePutRequest()
+    {
+        return new botwebapi\JsonHttpResponse(405, $_SERVER['REQUEST_METHOD'].' is not supported');
+    }
+    
+    protected function handleDeleteRequest()
+    {
+        return new botwebapi\JsonHttpResponse(405, $_SERVER['REQUEST_METHOD'].' is not supported');
+    }
+    
+    protected function getChild($name)
+    {
+        return NULL;
+    }
+}
+
+?>
--- botwebapi/classes/botwebapi/resources/api/projects/Projects.php
+++ botwebapi/classes/botwebapi/resources/api/projects/Projects.php
@@ -7,17 +7,23 @@ use botwebapi as botwebapi;
 class ProjectDescriptor
 {
     public $name;
+    public $location;
     public $uri;
+    public $about;
     
-    public function __construct($name, $uri)
+    public function __construct($name, $uri, $location, $about)
     {
         $this->name = $name;
         $this->uri = $uri;
+        $this->location = $location;
+        $this->about = $about;
     }
 }
 
 class Projects extends resources\BotWebApiResource
 {
+    const PROJECTS_ROOT_DIR = '/kovan/archives';
+    
     public function __construct($uri)
     {
         parent::__construct('projects', $uri, '1.0', 'https://github.com/kipr/botwebapi');
@@ -25,7 +31,28 @@ class Projects extends resources\BotWebApiResource
     
     protected function handleGetRequest()
     {
-        return new botwebapi\JsonHttpResponse(405, $_SERVER['REQUEST_METHOD'].' is not supported');
+        // returns a list of all child resources
+        $project_descriptors = array();
+        foreach (glob(Projects::PROJECTS_ROOT_DIR.'/*') as $file)
+        {
+            // Link projects are compressed and stored in a single archive
+            if(!is_dir($file))
+            {
+                // create the resource
+                $resource_name = basename($file);
+                $resource = $this->getChild($resource_name);
+                
+                // create the resource descriptor
+                $project_descriptor = new ProjectDescriptor($resource->getName(),
+                                                            $resource->getUri(),
+                                                            $resource->getLocation(),
+                                                            array('version' => $resource->getVersion(),
+                                                                  'homepage' => $resource->getHomepage()));
+                array_push($project_descriptors, $project_descriptor);
+            }
+        }
+        
+        return new botwebapi\JsonHttpResponse(200, array('projects' => $project_descriptors));
     }
     
     protected function handlePostRequest()
@@ -45,8 +72,18 @@ class Projects extends resources\BotWebApiResource
     
     protected function getChild($name)
     {
-        return NULL;
+        try
+        {
+            // Load the resource. The class name is <this namespace>\<name>\<Name>
+            $resource_class_name = __NAMESPACE__.'\\Project';
+            return new $resource_class_name($name,
+                                            $this->getUri().'/'.$name,
+                                            Projects::PROJECTS_ROOT_DIR.DIRECTORY_SEPARATOR.$name);
+        }
+        catch(\Exception $e)
+        {
+            return NULL;
+        }
     }
 }
-
 ?>
