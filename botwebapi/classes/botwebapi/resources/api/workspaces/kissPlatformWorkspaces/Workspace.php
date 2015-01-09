3629e4f975a763e2d1350a46e792b37760e33037
Stefan Zeltner <zeltner@usc.edu>
2015-01-09 13:03:33 -0600

    Implement workspace post

--- botwebapi/classes/botwebapi/resources/api/workspaces/kissPlatformWorkspaces/Workspace.php
+++ botwebapi/classes/botwebapi/resources/api/workspaces/kissPlatformWorkspaces/Workspace.php
@@ -47,7 +47,62 @@ class Workspace extends resources\BotWebApiResource
     
     public function post($content)
     {
-        return new botwebapi\JsonHttpResponse(501, 'Not implemented yet');
+        $json_data = json_decode($content, true);
+        if(!array_key_exists('name', $json_data))
+        {
+            return new botwebapi\JsonHttpResponse(422, 'Parameter "name" required');
+        }
+        $new_project_name = $json_data["name"];
+        
+        // check for duplicates
+        foreach(glob(ARCHIVES_ROOT_DIR.DIRECTORY_SEPARATOR.'*') as $file)
+        {
+            // Link projects are compressed and stored in a single archive
+            if(!is_dir($file))
+            {
+                $project_name = basename($file);
+                if($project_name == $new_project_name)
+                {
+                    return new botwebapi\JsonHttpResponse(409, $new_project_name.' already exists');
+                }
+            }
+        }
+        
+        // create a temporary directory
+        $temp_dir = sys_get_temp_dir().'/kiss_'.uniqid();
+        while(file_exists($temp_dir))
+        {
+	        $temp_dir = sys_get_temp_dir().'/kiss_'.uniqid();
+        }
+        
+        if(mkdir($temp_dir))
+        {
+	        $temp_kar_file = $this->temp_dir.'/'.basename($this->path);
+	        
+	        // create ops file
+	        $ops_file = $temp_dir.DIRECTORY_SEPARATOR.$new_project_name.'.ops';
+	        $ops_content = '';
+	        file_put_contents($ops_file, $ops_content);
+	        
+	        // create kam file
+	        $kam_file = $temp_dir.DIRECTORY_SEPARATOR.$new_project_name.'.kam';
+	        $kam_content = $ops_file.' => '.$new_project_name.'.ops'.PHP_EOL;
+	        file_put_contents($kam_file, $kam_content);
+	        
+            // package it
+	        exec('cd '.$temp_dir.' && kissarchive -c '.$new_project_name.' 1 '.$kam_file);
+	        
+	        // copy it into the archives folder
+	        copy($temp_dir.DIRECTORY_SEPARATOR.$new_project_name.'-1.kiss', ARCHIVES_ROOT_DIR.DIRECTORY_SEPARATOR.$new_project_name);
+            
+            // delete the temp file
+            Workspace::delTree($temp_dir);
+            
+            $location = botwebapi\LinksObject::buildUri($this->getResourceUri().'/'.urlencode($new_project_name));
+            return new botwebapi\HttpResponse(201, '', array('Location' => $location));
+        } else {
+            return new botwebapi\JsonHttpResponse(500, 'Unable to create temporary directory');
+        }
     }
     
     public function put($content)
@@ -71,6 +126,17 @@ class Workspace extends resources\BotWebApiResource
         {
             return NULL;
         }
+    }
+    
+    // from http://php.net/manual/de/function.rmdir.php
+    private static function delTree($dir)
+    { 
+        $files = array_diff(scandir($dir), array('.','..')); 
+        foreach ($files as $file)
+        { 
+            (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file"); 
+        } 
+        return rmdir($dir); 
     }
 }
 
